e146aa560f818b8c3ddeb9bd9104cff89bb09658
To Oak <casioaokcode@gmail.com>
2026-06-30 22:32:27 +0700

    Should email containing non-English characters and invalid format

---
+++ tests/loginPage01.spec.ts
@@ -0,0 +1,46 @@
+import {test, expect} from '@playwright/test'
+
+const loginUrl:string = 'https://automate-test.stpb-digital.com/login/'
+
+let loginEmailInput:string = '#email'
+let loginPasswordInput:string = 'input[name="password"]'
+let loginbtn:string = '#btn-login'
+
+//msg
+let loginmsgEmail:string = 'email must be a valid email'
+
+test.beforeEach('Open Url', async({page}) => {
+    await page.goto(loginUrl)
+})
+
+
+test('Verify Login page elements displayed are correctly', async ({page}) => {
+    await expect(page.getByRole('heading', {name:"Welcome to Kru P' Beam! 👋🏻"})).toBeVisible()
+    await expect(page.locator(loginEmailInput)).toBeVisible()
+    await expect(page.locator(loginPasswordInput)).toBeVisible()
+    await expect(page.locator(loginbtn)).toBeEnabled()
+    await expect(page.getByRole('link', {name:"Create an account"})).toBeVisible
+})
+
+test('Verify display name page',async ({page}) => {
+    await expect(page).toHaveTitle("Kru P' Beam - STPB")
+})
+
+test('Verify login page elements displayed correctly', async ({page}) => {
+    await page.locator(loginEmailInput).fill("user.test@krupbeam.com")
+    await page.locator(loginbtn).click()
+
+    await expect(page.getByText(loginmsgEmail)).not.toBeVisible()
+})
+
+test.only('Should email containing non-English characters and invalid format',async ({page}) => {
+    
+    let data:string[] = ['user.krupbeam.com', 'ไทย','09912345678']
+
+    for(let x of data){
+        await page.locator(loginEmailInput).fill(x)
+        await page.locator(loginbtn).click()
+
+        await expect(page.getByText(loginmsgEmail)).toBeVisible()
+    }
+})
