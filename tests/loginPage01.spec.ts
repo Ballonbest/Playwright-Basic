2ef33dd99fb5ed91ef13060fa3883a5358ea2a96
To Oak <casioaokcode@gmail.com>
2026-07-12 09:49:03 +0700

    add additional test

--- tests/loginPage01.spec.ts
+++ tests/loginPage01.spec.ts
@@ -1,66 +1,108 @@
-import { test, expect } from '@playwright/test'
+import {test , expect} from '@playwright/test'
 
-const loginUrl: string = 'https://automate-test.stpb-digital.com/login/'
+let loginemailInput:string = '#email'
+let loginPasswordInput:string = 'input[name="password"]'
 
-let loginEmailInput: string = '#email'
-let loginPasswordInput: string = 'input[name="password"]'
-let loginbtn: string = '#btn-login'
+let loginbtn:string = '#btn-login'
 
-//msg
-let loginmsg_Email: string = 'email must be a valid email'
-let loginmsg_Password: string = 'password must be at least 5 characters'
-
-test.beforeEach('Open Url', async ({ page }) => {
-    await page.goto(loginUrl)
+test.beforeEach('open URL', async({page}) => {
+    await page.goto("https://automate-test.stpb-digital.com/login/")
 })
 
-
-test('Verify Login page elements displayed are correctly', async ({ page }) => {
-    await expect(page.getByRole('heading', { name: "Welcome to Kru P' Beam! 👋🏻" })).toBeVisible()
-    await expect(page.locator(loginEmailInput)).toBeVisible()
-    await expect(page.locator(loginPasswordInput)).toBeVisible()
-    await expect(page.locator(loginbtn)).toBeEnabled()
-    await expect(page.getByRole('link', { name: "Create an account" })).toBeVisible
+test('Should display all login page elements', async({page}) => {
+    await page.getByText("Welcome to Kru P' Beam! 👋🏻")
+    await expect(page.locator(loginemailInput)).toBeEditable()
+    await expect(page.locator(loginPasswordInput)).toBeEditable()
+    await expect(page.locator(loginbtn)).toBeVisible()
+    await expect(page.getByRole('link', {name:"Create an account"})).toBeVisible()
 })
 
-test('Verify display name page', async ({ page }) => {
-    await expect(page).toHaveTitle("Kru P' Beam - STPB")
+test('Should display correct page title', async({page}) => {
+    await page.getByTitle("Kru P' Beam - STPB")
 })
 
-test('Valid Email with Special Characters', async ({ page }) => {
-    await page.locator(loginEmailInput).fill('user.test@krupbeam.com')
+test('Should accept valid email format', async({page}) => {
+    await page.locator(loginemailInput).fill("user.test@krupbeam.com")
+    await expect(page.locator(loginemailInput)).toHaveValue("user.test@krupbeam.com")
 
     await page.locator(loginbtn).click()
-    await expect(page.getByText(loginmsg_Email)).not.toBeVisible()
+    await expect(page.getByText("email must be a valid email")).not.toBeVisible()
+    await expect(page.locator('p.MuiFormHelperText-root', {hasText:"email must be a valid email"})).not.toBeVisible()
 })
 
-test('Should reject email containing non-English characters and invlid format', async ({ page }) => {
-    const data: string[] = ['user.krupbeam.com', 'ไทย', '09912345678']
-
-    for (let x of data) {
-        await page.locator(loginEmailInput).fill(x)
-        await page.locator(loginbtn).click()
-        await expect(page.getByText(loginmsg_Email)).toBeVisible()
-    }
-})
-
-test.only('Valid input over 5 digits', async ({ page }) => {
-    await page.locator(loginPasswordInput).fill('1234567890')
-
-    await expect(page.locator(loginPasswordInput)).toHaveValue('1234567890')
-    await expect(page.getByText(loginmsg_Password)).not.toBeVisible()
-})
-
-test.only('Invalid input under 5 digits',async ({page}) => {
-    let data:string[] = ['123','test']
+test('Should reject invalid email format', async({page}) => {
+    let data:string[] = ['user.krupbeam.com','ไทย','09912345678']
 
     for(let x of data){
-    await page.locator(loginPasswordInput).fill(x)
-    await expect(page.locator(loginPasswordInput)).toHaveValue(x)
+        await page.locator(loginemailInput).fill(x)
+        await page.locator(loginbtn).click()
+
+        await expect(page.getByText("email must be a valid email")).toBeVisible()
+        await expect(page.locator('p.MuiFormHelperText-root', {hasText:"email must be a valid email"})).toBeVisible()
+    }
+})
+
+test('Should accept password visibility', async ({page}) => {
+    await page.locator(loginPasswordInput).fill('1234567890')
     await page.locator(loginbtn).click()
 
-    await expect(page.getByText(loginmsg_Password)).toBeVisible()
-    }
+    await expect(page.getByText('password must be at least 5 characters')).not.toBeVisible()
+    await expect(page.locator('p.MuiFormHelperText-root', {hasText:"password must be at least 5 characters"})).not.toBeVisible()
+})
 
-    
+test('Should reject password under 5 characters', async({page}) => {
+    let passinvalid:string[] = ['123', 'test']
+
+    for(let x of passinvalid){
+        await page.locator(loginPasswordInput).fill(x)
+        await page.locator(loginbtn).click()
+
+        await expect(page.getByText('password must be at least 5 characters')).toBeVisible()
+        await expect(page.locator('p.MuiFormHelperText-root', {hasText:"password must be at least 5 characters"})).toBeVisible()
+    }
+})
+
+test('Should toggle password visibility', async({page}) => {
+    await page.locator(loginPasswordInput).fill('123')
+    await expect(page.locator(loginPasswordInput)).toHaveValue('123')
+    await page.locator('div:has(> input[name="password"]) button').click()
+
+    await expect(page.locator(loginPasswordInput)).toHaveAttribute('type', 'text')
+})
+
+test('Should login successfully with valid credentials', async({page}) => {
+    await page.locator(loginemailInput).fill('user.test@krupbeam.com')
+    await page.locator(loginPasswordInput).fill('jKNsrapwLNV7eBN')
+    await page.locator(loginbtn).click()
+
+    await page.waitForURL('https://automate-test.stpb-digital.com/user/list/')
+    await expect(page).toHaveURL('https://automate-test.stpb-digital.com/user/list/')
+    await expect(page.getByText('Search Filters')).toBeVisible()
+})
+
+test('Should reject unregistered email and password', async({page}) => {
+    await page.locator(loginemailInput).fill('test12345678@gmail.com')
+    await page.locator(loginPasswordInput).fill('12345678900')
+    await page.locator(loginbtn).click()
+
+    await expect(page.getByText('Email or Password is invalid')).toBeVisible()
+    await expect(page.locator('p.MuiFormHelperText-root', {hasText:"Email or Password is invalid"})).toBeVisible()
+})
+
+test.only('Should show required field errors when empty', async({page}) => {
+    await page.locator(loginemailInput).fill('')
+    await page.locator(loginPasswordInput).fill('')
+    await page.locator(loginbtn).click()
+
+    await expect(page.getByText("email is a required field")).toBeVisible()
+    await expect(page.getByText("password must be at least 5 characters")).toBeVisible()
+})
+
+test.only('Should navigate to signup page', async({page}) => {
+    await page.getByRole('link' , {name:"Create an account"}).click()
+    await page.waitForURL('https://automate-test.stpb-digital.com/register/')
+    await expect(page).toHaveURL("https://automate-test.stpb-digital.com/register/")
+
+    await expect(page).toHaveTitle("Kru P' Beam - STPB")
+    await expect(page.getByText("Welcome to Kru P' Beam! 👋🏻").first()).toBeVisible()
 })
